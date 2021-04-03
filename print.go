d2045302aa4c65dae9317db10848674d0d14931c
Moritz Poldrack <ich@moritz-poldrack.de>
2021-04-03 16:10:45 +0200

    fixed small bugs

--- print.go
+++ print.go
@@ -134,7 +134,7 @@ func Fatalf(format string, values ...interface{}) {
 
 func getLogLine(lvl Level, message string) string {
 	if showCaller[lvl] {
-		return fmt.Sprintf(logFormatCaller, lvl.Short(), time.Now().Format(TimeFormat), getCaller(4), message)
+		return fmt.Sprintf(logFormatCaller, lvl.Short(), time.Now().Format(TimeFormat), getCaller(2), message)
 	}
 	return fmt.Sprintf(logFormat, lvl.Short(), time.Now().Format(TimeFormat), message)
 }
--- settings.go
+++ settings.go
@@ -8,9 +8,9 @@ var (
 	// default is ISO-8601 which corresponds to:
 	TimeFormat = "2006-01-02T15:04:05-0700"
 	//           Level:\tTimestamp – Message
-	logFormat = "%s:\t%s – %s"
+	logFormat = "%s:\t%s – %s\n"
 	//                 Level:\tTimestamp – Caller – Message
-	logFormatCaller = "%s:\t%s – %s – %s"
+	logFormatCaller = "%s:\t%s – %s – %s\n"
 	// showCaller contains whether a certain loglevel should show it's caller.
 	showCaller = [...]bool{
 		true,  // TRACE
