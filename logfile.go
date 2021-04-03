3550f215100afb7431d61fecb7472713e919b2fe
Moritz Poldrack <ich@moritz-poldrack.de>
2021-04-03 18:09:45 +0200

    added more documentation

--- logfile.go
+++ logfile.go
@@ -4,13 +4,12 @@ import (
 	"os"
 )
 
+// AddLogFile opens the specified files and adds it to the specified levels
 func AddLogFile(filepath string, lvls ...Level) (*os.File, error) {
 	fh, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY|os.O_SYNC|os.O_CREATE, 0o600)
 	if err != nil {
 		return nil, err
 	}
-
-	/*bufw := bufio.NewWriter(fh)*/
 
 	for _, lvl := range lvls {
 		AddOutput(lvl, fh)
--- settings.go
+++ settings.go
@@ -4,6 +4,9 @@ var (
 	// TimeFormat specifies the formatting of the timestamp to use. The
 	// default is ISO-8601 which corresponds to:
 	TimeFormat = "2006-01-02T15:04:05-0700"
+	// NoPanic indicates that after a call to Fatal(f) no panic-stop shall
+	// be executed.
+	NoPanic bool
 	//           Level:\tTimestamp – Message
 	logFormat = "%s:\t%s – %s\n"
 	//                 Level:\tTimestamp – Caller – Message
@@ -19,6 +22,8 @@ var (
 	}
 )
 
+// SetShowCaller allows defining for what levels the caller is displayed in the
+// Log. By default the caller is shown for TRACE, DEBUG, ERROR, and FATAL.
 func SetShowCaller(lvl Level, show bool) {
 	if !isValid(lvl) {
 		return
