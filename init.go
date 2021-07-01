db092d7411884db6ea384dfe0712b05c0bca0a38
Moritz Poldrack <ich@moritz-poldrack.de>
2021-07-01 15:21:10 +0200

    warning and above are now output to stdout if connected to a file

--- init.go
+++ init.go
@@ -1,9 +1,20 @@
 package glog
 
-import "git.sr.ht/~poldi1405/go-ansi"
+import (
+	"os"
+
+	"git.sr.ht/~poldi1405/go-ansi"
+)
 
 // init attempts to automatically enable ANSI-Escape-Sequences on Windows
 // Consoles. I just can't bring myself to calling them "terminals"
 func init() {
 	ansi.EnableANSI()
+	// if stdout is a file, add stdout to all levels. the user probably
+	// wants all logmessages
+	if fi, err := os.Stdout.Stat(); err == nil {
+		if fi.Mode().IsRegular() {
+			AddOutputAbove(WARNING, os.Stdout)
+		}
+	}
 }
