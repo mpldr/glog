f1c44799193675feb567d987ba7b54e862796ffd
Moritz Poldrack <ich@moritz-poldrack.de>
2021-07-04 14:51:55 +0200

    added docs for logrotation

---
+++ logrotation/docs.go
@@ -0,0 +1,4 @@
+// Package logrotation provides a writer that automatically rotates logfiles
+// if the set maximum size is reached or exceeded. Compression is enabled by
+// default, but can be disabled if required.
+package logrotation
